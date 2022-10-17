0fc941d4469e77f20857c61ec03bab8da2856aa8
FlipWarthog <flipwarthog@gmail.com>
2022-10-16 22:55:57 -0400

    Add join JPA

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,5 +1,9 @@
-insert into TestJpa (id, stringCol, numberCol, decimalCol, dateCol) values (1, 'New York', 123, 123.45, PARSEDATETIME('2022-10-01','yyyy-MM-dd'));
-insert into TestJpa (id, stringCol, numberCol, decimalCol, dateCol) values (2, 'Los Angeles', 234, 234.45, PARSEDATETIME('2022-10-01','yyyy-MM-dd'));
-insert into TestJpa (id, stringCol, numberCol, decimalCol, dateCol) values (3, 'Philladelphia', 345, 345.45, PARSEDATETIME('2022-10-01','yyyy-MM-dd'));
-insert into TestJpa (id, stringCol, numberCol, decimalCol, dateCol) values (4, 'Detroit', 456, 456.45, PARSEDATETIME('2022-10-01','yyyy-MM-dd'));
-insert into TestJpa (id, stringCol, numberCol, decimalCol, dateCol) values (5, 'Boston', 567, 567.45, PARSEDATETIME('2022-10-01','yyyy-MM-dd'));
+
+insert into ChildJpa (id, stringCol, numberCol, decimalCol, dateCol) values (1, 'Child 1', 456, 456.45, PARSEDATETIME('2022-10-01','yyyy-MM-dd'));
+insert into ChildJpa (id, stringCol, numberCol, decimalCol, dateCol) values (2, 'Child 2', 456, 456.45, PARSEDATETIME('2022-10-01','yyyy-MM-dd'));
+
+insert into TestJpa (id, stringCol, numberCol, decimalCol, dateCol, child_id) values (1, 'New York', 123, 123.45, PARSEDATETIME('2022-10-01','yyyy-MM-dd'), 1);
+insert into TestJpa (id, stringCol, numberCol, decimalCol, dateCol, child_id) values (2, 'Los Angeles', 234, 234.45, PARSEDATETIME('2022-10-01','yyyy-MM-dd'), 1);
+insert into TestJpa (id, stringCol, numberCol, decimalCol, dateCol, child_id) values (3, 'Philladelphia', 345, 345.45, PARSEDATETIME('2022-10-01','yyyy-MM-dd'), 1);
+insert into TestJpa (id, stringCol, numberCol, decimalCol, dateCol, child_id) values (4, 'Detroit', 456, 456.45, PARSEDATETIME('2022-10-01','yyyy-MM-dd'), 2);
+insert into TestJpa (id, stringCol, numberCol, decimalCol, dateCol, child_id) values (5, 'Boston', 567, 567.45, PARSEDATETIME('2022-10-01','yyyy-MM-dd'), 2);
